8c4efe5a51bf24d3cf2256905d8b5be93966bfa1
katsudouki <katsudouki@outlook.com.br>
2025-01-28 18:56:15 -0300

    🔧 ci: update markdown-preview plugin configuration

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,11 +1,12 @@
-return {
-	-- INFO: plugin vizualizador de arquivo markdown
-   "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",  -- Comando para instalar as dependências do plugin
-    ft = { "markdown" },  -- Carregar o plugin apenas para arquivos markdown
-    config = function()
-        -- Associa as extensões .MD e .md ao tipo de arquivo markdown
-        vim.api.nvim_command('au BufRead,BufNewFile *.MD set filetype=markdown')
-        vim.api.nvim_command('au BufRead,BufNewFile *.md set filetype=markdown')
-    end
-}
+return 
+{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install && git restore .",
+    -- or if you use yarn: (I have not checked this, I use npm)
+    -- build = "cd app && yarn install && git restore .",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  }
