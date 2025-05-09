01275355f30b40b0d348f96fe9594a03b2b6effa
katsudouki <katsudouki@outlook.com.br>
2025-05-09 01:01:33 -0300

    🌟 feat: added plugin for compile with f5 key
    Changelog: feature

--- init.lua
+++ init.lua
@@ -84,7 +84,7 @@ require('nvim-treesitter.configs').setup {
     },
     move = {
       enable = true,
-      set_jumps = true, 
+      set_jumps = true,
       goto_next_start = {
         [']m'] = '@function.outer',
         [']]'] = '@class.outer',
@@ -157,21 +157,21 @@ local servers = {
 require('neodev').setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+-- INFO: configura servidores LSP com mason-lspconfig (sem setup_handlers)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
+for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
+  require('lspconfig')[server_name].setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = servers[server_name],
+  }
+end
+
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
@@ -192,7 +192,7 @@ cmp.setup {
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
-      completion = {completeopt = 'menuone,noselect'}, 
+      completion = {completeopt = 'menuone,noselect'},
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
@@ -233,7 +233,7 @@ vim.o.cmdheight = 0
 vim.opt.termguicolors = true
 vim.cmd([[hi NvimTreeNormalNC guibg=NONE]])
 vim.cmd([[highlight NvimTreeNormal guibg=NONE ctermbg=NONE]])
-vim.g.nvim_tree_highlight_opened_files = 1 
+vim.g.nvim_tree_highlight_opened_files = 1
 
 -- INFO: habilita os cursores vertical e horizontal
 vim.api.nvim_set_option('cursorcolumn', true)
